3427e6b294882962c3a440f8fe5390c315f2f611
chumak-av <57865550+chumak-av@users.noreply.github.com>
2020-05-11 23:37:46 +0300

    1

---
+++ module-5/task5/index.js
@@ -0,0 +1,5 @@
+const message = 'Welcome to Ukraine!';
+const messageArray = message.split('');
+console.log(message.split(''));
+console.log(message.indexOf('l'));
+console.log(messageArray.join(''));
---
+++ module-5/task8/index.js
@@ -0,0 +1,3 @@
+const animals = ['pig', 'dog', 'cat', 'parrot', 'chicken'];
+const pets = animals.splice(1, 3);
+console.log(animals, pets);
